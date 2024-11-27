e8317fc578b2aab74255566c0d5dac9b08eac0bf
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-11-27 02:23:12 -0300

    Update Methods.java

--- Biblioteca/src/Methods.java
+++ Biblioteca/src/Methods.java
@@ -10,11 +10,11 @@ public class Methods {
         System.out.printf("%-3s | %-20s | %-20s | %-20s\n", "ID", "Titulo", "Autor", "Status");
         System.out.println("----------------------------------------------------------");
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i).rented == false) {
-                System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(i).id, books.get(i).title, books.get(i).author, "Livre");
+            if (books.get(i).isRented() == false) {
+                System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(i).getId(), books.get(i).getTitle(), books.get(i).getAuthor(), "Livre");
             }
             else {
-                System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(i).id, books.get(i).title, books.get(i).author, "Alugado");
+                System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(i).getId(), books.get(i).getTitle(), books.get(i).getAuthor(), "Alugado");
             }
         }
     }
@@ -22,7 +22,13 @@ public class Methods {
     public static void show(ArrayList<Book> books, int index) {
         System.out.printf("%-3s | %-20s | %-20s | %-20s\n", "ID", "Titulo", "Autor", "Status");
         System.out.println("---------------------------------------------------");
-        System.out.printf("%-3d | %-20s | %-20s\n", books.get(index).id, books.get(index).title, books.get(index).author);
+        if (books.get(index).isRented() == false) {
+            System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(index).getId(), books.get(index).getTitle(), books.get(index).getAuthor(), "Livre");
+        }
+        else {
+            System.out.printf("%-3d | %-20s | %-20s | %-20s\n", books.get(index).getId(), books.get(index).getTitle(), books.get(index).getAuthor(), "Alugado");
+        }
+
     }
 
     //Função para ordenar o livros por nome
@@ -30,7 +36,7 @@ public class Methods {
         Book temp;
         for (int i = 0; i < books.size(); i++) {
             for (int j = 0; j < books.size() - 1 - i; j++) {
-                if (books.get(j).title.compareTo(books.get(j + 1).title) > 0) {
+                if (books.get(j).getTitle().compareTo(books.get(j + 1).getTitle()) > 0) {
                     temp = books.get(j);
                     books.set(j, books.get(j + 1));
                     books.set(j + 1, temp);
@@ -45,7 +51,7 @@ public class Methods {
 
     public static void update(String title, ArrayList<Book> books) {
         for (int  i = 0; i < books.size(); i++) {
-            if (title.equals(books.get(i).title)) {
+            if (title.equals(books.get(i).getTitle())) {
                 System.out.println("O que deseja editar?");
                 int input;
                 do {
@@ -59,7 +65,7 @@ public class Methods {
                         case 1:
                             System.out.printf("Digite o novo titulo: ");
                             String newTitle = scanner.nextLine();
-                            books.get(i).title = newTitle;
+                            books.get(i).setTitle(newTitle);
                             System.out.println("Titulo alterado com sucesso");
                             show(books, i);
                             break;
@@ -67,7 +73,7 @@ public class Methods {
                         case 2:
                             System.out.printf("Digite o novo autor: ");
                             String newAuthor = scanner.nextLine();
-                            books.get(i).title = newAuthor;
+                            books.get(i).setTitle(newAuthor);
                             System.out.println("Autor alterado com sucesso");
                             show(books, i);
                             break;
@@ -89,8 +95,8 @@ public class Methods {
         }
 
         for (int i = 0; i < books.size(); i++) {
-            if (input.equals(books.get(i).title)) {
-                System.out.printf("Voce tem certeza que deseja excluir %s ('y'/'n')?\n", books.get(i).title);
+            if (input.equals(books.get(i).getTitle())) {
+                System.out.printf("Voce tem certeza que deseja excluir %s ('y'/'n')?\n", books.get(i).getTitle());
                 char choice = scanner.next().charAt(0);
                 if (choice == 'n') {
                     System.out.println("Ação cancelada");
