ecfd08dbb8eaeeb40b40598aa50d497e29bd7ece
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-12-01 03:27:36 -0300

    Update Book.java
    update

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+public class Book {
+    private String title;
+    private String author;
+    private int id;
+    private boolean rented;
 
-public class Livros {
+
+    public Book(String title, String author, int id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+        this.rented = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isRented() { return rented; }
+
+    public void setRented(boolean isRented) {
+        this.rented = isRented;
+    }
 }
